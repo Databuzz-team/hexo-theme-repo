bd512b93c8acdad4e456c316347437017d2e5ea4
DanialDaeHyunNam <danialnam89@gmail.com>
2018-12-03 16:02:02 +0900

    Not tracking data when it's localhost

--- source/js/custom.js
+++ source/js/custom.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-127200932-1');
+  var url = window.location.href;     // Returns full URL
+  if(!url.includes("localhost:4000")){
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-127200932-1');
+  }
 
   var scrollTrigger = 500;
 
